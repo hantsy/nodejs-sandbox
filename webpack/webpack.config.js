e7bb8dbfa3940a53b89255a95ab72fce9e76e016
hantsy <hantsy@gmail.com>
2019-10-18 16:44:54 +0800

    update webpack.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
@@ -6,5 +7,33 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'build')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.jpe?g$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 10000 }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimize: true //Update this to true or false
+  },
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin(),
+    new webpack.ContextReplacementPlugin()
+  ]
 };
